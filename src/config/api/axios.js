4513de0a209f5fda22cdfde6b2653003c0c86ba0
srfscodteam <86787296+generalgerry@users.noreply.github.com>
2023-12-14 06:46:51 +0000

    axios.js

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://kollege-api.onrender.com",
+  baseURL: "https://highend.onrender.com",
   // "http://localhost:3500",
 
   headers: { "Content-Type": "application/json" },
